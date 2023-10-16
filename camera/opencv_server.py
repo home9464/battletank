945cb776b9d0f8c99ffa956868c011e08420a7a2
Ying Sun <home9464@gmail.com>
2023-10-16 11:00:17 -0700

    ok

--- camera/opencv_server.py
+++ camera/opencv_server.py
@@ -13,17 +13,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return render_template('index.html')
+    return render_template('./index.html')
 
 def gen():
     """Video streaming generator function."""
     vs = cv2.VideoCapture(0)
     while True:
         ret,frame=vs.read()
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        frame=jpeg.tobytes()
-        yield (b'--frame\r\n'
-        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        if ret==True and frame is not None:
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            frame=jpeg.tobytes()
+            yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         
     vs.release()
     cv2.destroyAllWindows() 
@@ -34,4 +35,4 @@ def video_feed():
     return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
